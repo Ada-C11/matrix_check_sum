ac7e7799fcfb545c9a08189f306402f9161eb791
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-28 19:48:05 -0700

    passes all tests

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,8 +3,26 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(m * n), where m is the number of rows in the matrix and n is the number of columns
+# Space complexity: O(1)
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  elements = matrix.length
+
+  i = 0
+
+  until i == elements
+    sum1 = 0
+    sum2 = 0
+    j = 0
+    until j == elements
+      sum1 += matrix[i][j]
+      sum2 += matrix[j][i]
+
+      j += 1
+    end
+    return false if sum1 != sum2
+    i += 1
+  end
+
+  return true
 end
