a507b08ca2fb289b42f09ef5da9fce8c9cfe6a5d
sjscotton <sj.scotton@gmail.com>
2019-04-29 20:25:48 -0700

    updated complexities

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,7 +3,7 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 
-# Time complexity: O(n) where n is the number of items in the matrix
+# Time complexity: O(n) where n is the width of the matrix in the matrix, it would be O(n+2n) which redices to O(n)
 # Space complexity: O(1) constant number of fixed sized variables are used regardless of the size of the martix
 def matrix_check_sum(matrix)
   rows = matrix.length
