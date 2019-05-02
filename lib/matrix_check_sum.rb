58d181133c7dc50c729f6604b4d5adc2677042ea
Heather Izumi <hkizumi@gmail.com>
2019-05-01 23:22:53 -0700

    passing all tests

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,8 +3,40 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) - n is the number of elements in the matrix
+# Space complexity: O(n) - n is the number of arrays in the matrix
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  row_sums = []
+  column_sums = []
+
+  row_total = 0
+  column_total = 0
+
+  column_index = 0
+  while column_index < matrix[0].length
+    row_index = 0
+    while row_index < matrix.length
+      column_total += matrix[row_index][column_index]
+      row_index += 1
+    end
+    column_sums << column_total
+    column_index += 1
+  end
+
+  row_index = 0
+  while row_index < matrix.length
+    column_index = 0
+    while column_index < matrix[0].length
+      row_total += matrix[row_index][column_index]
+      column_index += 1
+    end
+    row_sums << row_total
+    row_index += 1
+  end
+
+  if row_sums == column_sums
+    return true
+  else
+    return false
+  end
 end
