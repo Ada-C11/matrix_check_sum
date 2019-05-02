29c8840e4da8fbc9bd341fdf418d7654d6e2c020
Kate Sandler <kate@katekatekate.net>
2019-05-02 03:03:14 -0700

    Method for matrix_check_sum

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -1,10 +1,16 @@
-# Checks that for the given matrix, where number of rows are equal to number of columns
-# whether the sum of each row matches the sum of corresponding column i.e. sum
-# of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
-# If this is the case, return true. Otherwise, return false.
-
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the number of items rows/cols in the matrix
+# Space complexity: O(1)
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  matrix.length.times do |row|
+    col_sum = 0
+    row_sum = 0
+    matrix.length.times do |col|
+        col_sum += matrix[row][col]
+        row_sum += matrix[col][row]
+    end
+    return false if col_sum != row_sum
+  end
+return true
 end
+
+matrix_check_sum(matrix)
