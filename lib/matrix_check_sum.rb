a5d1a3053d62ae618aad122c77c251e22c0afd1d
Erica Norris <norrise120@gmail.com>
2019-05-02 09:20:58 -0700

    wrote method to pass tests and added time/space complexity

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,8 +3,30 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), where n is the number of rows (since # of columns and rows are assumed to be the same)
+# Space complexity: O(n), where n is the number of rows
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  length = matrix.length
+  row_sum = 0
+  col_sum = 0
+  hash = {}
+
+  length.times do |x|
+    length.times do |y|
+      row_sum += matrix[x][y]
+    end
+
+    hash[x] = row_sum
+  end
+
+  length.times do |y|
+    length.times do |x|
+      col_sum += matrix[x][y]
+    end
+
+    hash[y] -= col_sum
+    return false if hash[y] != 0
+  end
+
+  return true
 end
